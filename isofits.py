0ae7582edb6d38b44f711b53157a20c09a13c62c
Parker Laptop <parkergreene16@gmail.com>
2023-12-02 19:07:31 -0800

    added isofit and isoreport functions

--- isofits.py
+++ isofits.py
@@ -4,11 +4,12 @@ from src.data import shaft_data
 import sys
 
 #Find iso tolerance
-def isofit(body, size, fit, side):
-    """Returns ISO tolerance of hole
+def isotol(body, size, fit, side):
+    """Returns ISO tolerance of hole/shaft
     Parameters:
-    size (float): Size of hole in mm
-    fit (str): ISO fit class of hole
+    body (str): 'hole' or 'shaft'
+    size (float): Size of body [mm]
+    fit (str): ISO fit class of body
     side (str): Select from tolerance band upper, lower, or both
     """
     if body == 'hole':
@@ -24,3 +25,32 @@ def isofit(body, size, fit, side):
         return find_tol(body, find_size_index(body,size), fit, side)
     else:
         raise ValueError('Invalid size, enter in range [3, 400]')
+    
+def isofit(size, hole_fit, shaft_fit):
+    """Returns MMC and LMC for ISO fit of hole/shaft
+    Parameters:
+    size (float): Size of body [mm]
+    hole_fit (str): ISO fit class of hole (ex: 'H7')
+    shaft_fit (str): ISO fit class of shaft (ex: 'h6')
+    """
+    hole_max, hole_min = isotol('hole', size, hole_fit, 'both')
+    shaft_max, shaft_min = isotol('shaft', size, shaft_fit, 'both')
+    mmc = hole_min - shaft_max
+    lmc = hole_max - shaft_min
+    return mmc, lmc
+
+def isoreport(size, hole_fit, shaft_fit): 
+    """Returns MMC and LMC for ISO fit of hole/shaft
+    Parameters:
+    size (float): Size of body [mm]
+    hole_fit (str): ISO fit class of hole (ex: 'H7')
+    shaft_fit (str): ISO fit class of shaft (ex: 'h6')
+    """
+    mmc, lmc = isofit(size, hole_fit, shaft_fit)
+    if mmc >=0 and lmc >=0:
+        fit_type = 'Clearance Fit'
+    elif mmc <0 and lmc <0:
+        fit_type = 'Interference Fit'
+    else:
+        fit_type = 'Transition Fit'
+    print(f'{size} {hole_fit}/{shaft_fit} | {fit_type} | {lmc}, {mmc}')
--- test.py
+++ test.py
@@ -5,15 +5,21 @@ from src.data import hole_data
 from src.data import shaft_data
 import numpy as np
 
-for _ in range(1000):
-    body = np.random.choice(['hole', 'shaft'])
-    size = np.random.uniform(3,100)
-    if body == 'hole':
-        fit = np.random.choice(create_fit_lst(hole_data))
-    elif body == 'shaft':
-        fit = np.random.choice(create_fit_lst(shaft_data))
-    else:
-        print('ruh roh raggy')
-    ans = isofit(body,size,fit,'both')
-    print(f'{body} | {size} | {fit} | both --> {ans}')
+def test_isotol(n):
+    for _ in range(n):
+        body = np.random.choice(['hole', 'shaft'])
+        size = np.random.uniform(3,100)
+        if body == 'hole':
+            fit = np.random.choice(create_fit_lst(hole_data))
+        elif body == 'shaft':
+            fit = np.random.choice(create_fit_lst(shaft_data))
+        else:
+            print('ruh roh raggy')
+        ans = isotol(body,size,fit,'both')
+        print(f'{body} | {size:0.2f} | {fit} | both --> {ans}')
 
+#test_isotol(1000)
+
+isofit(10, 'H7', 'h6')
+
+isoreport(10, 'H7', 'h6')
